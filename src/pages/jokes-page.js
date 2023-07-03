4475f927bff96ad5fb2f6e7d4937954722e4d85f
jitenderattrimob <jitenderattri16@gmail.com>
2023-07-03 17:13:29 +0530

    file rename

--- src/pages/jokes-page.js
+++ src/pages/jokes-page.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import apiService from "../services/http-services";
+import apiService from "../services/api-service";
 import { Footer, JokeList, Wrapper } from "../components";
 import { JokesContext } from "../App";
 import { useNavigate } from "react-router-dom";
